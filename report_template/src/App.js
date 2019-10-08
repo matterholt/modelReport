3c4203704ac366ed758c806e361e37de2f79e142
MAtterholt <matterholt84@gmail.com>
2019-10-08 00:51:59 -0400

    create add cm info  page

--- report_template/src/App.js
+++ report_template/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 ////   Style
 import "./App.css";
 import PrintableDoc from "./print_doc/PrintableFormat";
+import AddData from "./components/AddCmModel";
 function PrintFEARequest() {
   const [showPrint, hidePrint] = useState(false);
   if (showPrint === false) {
@@ -20,11 +21,36 @@ function PrintFEARequest() {
   }
 }
 
+function DisplayData({ doc_Model }) {
+  const cmInfo = doc_Model.map((x, y) => {
+    return (
+      <>
+        <li>{x.cmModelName}</li>
+        <li>{x.baseName}</li>
+        <li>{x.modelWeight}</li>
+        <li>{x.description}</li>
+      </>
+    );
+  });
+  return <ul>{cmInfo}</ul>;
+}
+
 function App() {
+  const [doc_Model, updateDocModel] = useState([]);
+
+  function addToModelData(modelData) {
+    console.log("Model has been added");
+    let tryME = [...doc_Model, modelData];
+    updateDocModel(tryME);
+  }
+
   return (
     <div>
       <h1>FEA request</h1>
       <PrintFEARequest />
+      <AddData addToModelData={addToModelData} />
+      <h1>find it</h1>
+      <DisplayData doc_Model={doc_Model} />
     </div>
   );
 }
---
+++ report_template/src/components/AddCmModel.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import "./AddCmModelStyle.css";
+
+function AddData({ addToModelData }) {
+  const [cmName, setCmName] = useState("");
+  const [baseModel, setBaseModel] = useState("");
+  const [modelWeight, setModelWeight] = useState("");
+  const [modelDescription, setModelDescription] = useState("");
+  const [cmModelObj, setCmModelObj] = useState({
+    cmName: "",
+    baseName: "",
+    modelWeight: "",
+    description: ""
+  });
+
+  function addDataObj() {
+    const cmModelData = {
+      cmModelName: cmName,
+      baseName: baseModel,
+      modelWeight: modelWeight,
+      description: modelDescription
+    };
+    setCmModelObj(cmModelData);
+  }
+  useEffect(() => {
+    addToModelData(cmModelObj);
+  }, [cmModelObj]);
+
+  function clear() {
+    setCmName("");
+    setBaseModel("");
+    setModelWeight("");
+    setModelDescription("");
+  }
+
+  return (
+    <div className="AddingField">
+      <h3> Add data for Counter Measure</h3>
+      <label>
+        Model Name:
+        <br />
+        <input value={cmName} onChange={e => setCmName(e.target.value)}></input>
+      </label>
+      <label>
+        Base Model:
+        <br />
+        <input
+          value={baseModel}
+          onChange={e => setBaseModel(e.target.value)}
+        ></input>
+      </label>
+      <label>
+        Model Weight (kg.):
+        <br />
+        <input
+          value={modelWeight}
+          onChange={e => setModelWeight(e.target.value)}
+        ></input>
+      </label>
+      <label>
+        Model Changes:
+        <br />
+        <textarea
+          rows="5"
+          cols="33"
+          value={modelDescription}
+          onChange={e => setModelDescription(e.target.value)}
+        ></textarea>
+      </label>
+      <div>
+        <button className="Button add" onClick={addDataObj}>
+          Add Model
+        </button>
+        <button className="Button clear" onClick={clear}>
+          clear
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default AddData;
