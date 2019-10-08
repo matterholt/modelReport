0233f71efd01dced8d432195e572c2ef73a371e8
MAtterholt <matterholt84@gmail.com>
2019-10-07 22:32:38 -0400

    show/hide fea Request

--- report_template/src/App.js
+++ report_template/src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 ////   Style
 import "./App.css";
 import PrintableDoc from "./print_doc/PrintableFormat";
+function PrintFEARequest() {
+  const [showPrint, hidePrint] = useState(false);
+  if (showPrint === false) {
+    return (
+      <div>
+        <button onClick={() => hidePrint(true)}>Fea request report</button>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <button onClick={() => hidePrint(false)}> Hide page</button>
+        <PrintableDoc />
+      </>
+    );
+  }
+}
 
 function App() {
   return (
     <div>
-      <PrintableDoc />
+      <h1>FEA request</h1>
+      <PrintFEARequest />
     </div>
   );
 }
