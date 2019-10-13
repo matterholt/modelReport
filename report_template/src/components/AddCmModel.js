a019f239c0303d86069d471fd075858d999274e2
MAtterholt <matterholt84@gmail.com>
2019-10-13 09:36:09 -0400

    add data hide show

--- report_template/src/components/AddCmModel.js
+++ report_template/src/components/AddCmModel.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./AddCmModelStyle.css";
 
 function AddData({ addToModelData }) {
+  const [showModelForm, hideModelForm] = useState(false);
   const [cmName, setCmName] = useState("");
   const [baseModel, setBaseModel] = useState("");
   const [modelWeight, setModelWeight] = useState("");
@@ -15,6 +16,7 @@ function AddData({ addToModelData }) {
       description: modelDescription
     };
     addToModelData(cmModelData);
+    clear();
   }
 
   function clear() {
@@ -23,51 +25,57 @@ function AddData({ addToModelData }) {
     setModelWeight("");
     setModelDescription("");
   }
-
-  return (
-    <div className="AddingField">
-      <h3> Add data for Counter Measure</h3>
-      <label>
-        Model Name:
-        <br />
-        <input value={cmName} onChange={e => setCmName(e.target.value)}></input>
-      </label>
-      <label>
-        Base Model:
-        <br />
-        <input
-          value={baseModel}
-          onChange={e => setBaseModel(e.target.value)}
-        ></input>
-      </label>
-      <label>
-        Model Weight (kg.):
-        <br />
-        <input
-          value={modelWeight}
-          onChange={e => setModelWeight(e.target.value)}
-        ></input>
-      </label>
-      <label>
-        Model Changes:
-        <br />
-        <textarea
-          rows="5"
-          cols="33"
-          value={modelDescription}
-          onChange={e => setModelDescription(e.target.value)}
-        ></textarea>
-      </label>
-      <div>
-        <button className="Button add" onClick={addDataObj}>
-          Add Model
-        </button>
-        <button className="Button clear" onClick={clear}>
-          clear
-        </button>
+  if (showModelForm === true) {
+    return (
+      <div className="AddingField">
+        <h3> Add data for Counter Measure</h3>
+        <label>
+          Model Name:
+          <br />
+          <input
+            value={cmName}
+            onChange={e => setCmName(e.target.value)}
+          ></input>
+        </label>
+        <label>
+          Base Model:
+          <br />
+          <input
+            value={baseModel}
+            onChange={e => setBaseModel(e.target.value)}
+          ></input>
+        </label>
+        <label>
+          Model Weight (kg.):
+          <br />
+          <input
+            value={modelWeight}
+            onChange={e => setModelWeight(e.target.value)}
+          ></input>
+        </label>
+        <label>
+          Model Changes:
+          <br />
+          <textarea
+            rows="5"
+            cols="33"
+            value={modelDescription}
+            onChange={e => setModelDescription(e.target.value)}
+          ></textarea>
+        </label>
+        <div>
+          <button className="Button add" onClick={addDataObj}>
+            Add Model
+          </button>
+          <button className="Button clear" onClick={() => hideModelForm(false)}>
+            Done
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <button onClick={() => hideModelForm(true)}> Add Model</button>;
+  }
 }
 
 export default AddData;
