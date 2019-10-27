e31ca7f77f0868f0319d5de66009e3c792b37c4b
MAtterholt <matterholt84@gmail.com>
2019-10-26 23:34:41 -0400

    model info hooks

--- report_template/src/components/FeaRequest.js
+++ report_template/src/components/FeaRequest.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-
-function ModelAnalysis() {
-  return <div></div>;
-}
+import ModelInfo from "./request/ModelInfo";
+import ModelChange from "./request/ModelChange";
+import ModelAnalysis from "./request/ModelAnalysis";
 
 function ButtonProcess() {
   return (
@@ -19,79 +18,6 @@ function ButtonProcess() {
   );
 }
 
-function ModelInfo() {
-  return (
-    <form className="max-w-lg bg-white flex flex-col rounded">
-      <h2 className="text-center font-bold text-lg m-5 "> Model INFO</h2>
-      <label className="flex flex-col my-2 mx-10 text-md text-blue-800 font-semibold">
-        Base Name:
-        <input className="shadow my-2 p-2" />
-      </label>
-      <label className="flex flex-col my-2 mx-10 text-md text-blue-800 font-semibold">
-        Model Name:
-        <input className="shadow my-2 p-2" />
-      </label>
-      <label className="flex flex-col my-2 mx-10 text-md text-blue-800 font-semibold">
-        Model Purpose:
-        <textarea className="shadow my-2 p-2" />
-      </label>
-      <ButtonProcess />
-    </form>
-  );
-}
-
-function ModelChange() {
-  const [changesall, addChages] = useState([
-    "test_item1",
-    "test_item2",
-    "test_item3"
-  ]);
-  const [change, updateChange] = useState("test");
-
-  function addItemChange(e) {
-    e.preventDefault();
-    addChages([...changesall, change]);
-    updateChange("");
-  }
-
-  return (
-    <form className=" w-full max-w-lg bg-white flex flex-col rounded">
-      <h2 className="text-center font-bold text-lg m-5 "> Model Changes</h2>
-      <label className="flex flex-col my-2 mx-10 text-md text-blue-800 font-semibold">
-        Model Image:
-        <input className="shadow my-2 p-2" />
-      </label>
-
-      <div className="flex items-center">
-        <label className="flex flex-col my-2 mx-10 text-md text-blue-800 font-semibold">
-          Model changes:
-          <input
-            value={change}
-            onChange={e => updateChange(e.target.value)}
-            className="shadow my-2 p-2 "
-          />
-        </label>
-        <button
-          onClick={addItemChange}
-          className="flex justify-center text-white w-10 h-10 rounded-full bg-blue-700 text-2xl"
-        >
-          {" "}
-          +{" "}
-        </button>
-      </div>
-      <div>
-        <h2 className="text-center border-b-2 p-2">Changes To Model</h2>
-        <ul className="bg-blue-200 rounded p-4 m-2">
-          {changesall.map((item, k) => {
-            return <li key={k}>{item}</li>;
-          })}
-        </ul>
-      </div>
-      <ButtonProcess />
-    </form>
-  );
-}
-
 function Request() {
   const [pgNum, setPgNum] = useState(1);
   /*
@@ -101,18 +27,37 @@ function Request() {
     return <ModelChange />;
   }
   */
-  return (
-    <div className="flex w-screen justify-around">
-      <ModelInfo />
-      <ModelChange />
-    </div>
-  );
+  return <div className="flex w-screen justify-around"></div>;
 }
 
 const FEARequest = () => {
+  const [modelInfo, setModelInfo] = useState({
+    baseName: "",
+    cmVersion: "",
+    cmPurpose: ""
+  });
+
+  const modelIfoCollect = (base, cm, reason) => {
+    setModelInfo({
+      baseName: base,
+      cmVersion: cm,
+      cmPurpose: reason
+    });
+  };
+
   return (
-    <div className="w-screen h-screen bg-gray-100 flex flex-col justify-start items-center">
-      <Request />
+    <div className="w-screen h-screen flex justify-around">
+      <ModelInfo modelIfoCollect={modelIfoCollect} />
+      <ModelChange />
+      <ModelAnalysis />
+      <button
+        onClick={() => {
+          console.log(modelInfo);
+        }}
+      >
+        {" "}
+        console
+      </button>
     </div>
   );
 };
---
+++ report_template/src/components/request/ModelInfo.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+function ModelInfo({ modelIfoCollect }) {
+  const [baseModel, setBaseModel] = useState("");
+  const [cmModelName, setCmModelName] = useState("");
+  const [reasonModel, setReasonModel] = useState("");
+
+  const compileModelInfo = e => {
+    e.preventDefault();
+    modelIfoCollect(baseModel, cmModelName, reasonModel);
+  };
+
+  return (
+    <form className="max-w-lg bg-gray-300 flex flex-col rounded">
+      <h2 className="text-center font-bold text-lg m-5 "> Model INFO</h2>
+
+      <label className="flex flex-col my-2 mx-10 text-md text-blue-800 font-semibold">
+        Base Name:
+        <input
+          value={baseModel}
+          onChange={e => setBaseModel(e.target.value)}
+          className="shadow my-2 p-2"
+        />
+      </label>
+
+      <label className="flex flex-col my-2 mx-10 text-md text-blue-800 font-semibold">
+        Model Name:
+        <input
+          value={cmModelName}
+          onChange={e => setCmModelName(e.target.value)}
+          className="shadow my-2 p-2"
+        />
+      </label>
+
+      <label className="flex flex-col my-2 mx-10 text-md text-blue-800 font-semibold">
+        Model Purpose:
+        <textarea
+          value={reasonModel}
+          onChange={e => setReasonModel(e.target.value)}
+          placeholder="Reason for cm"
+          className="shadow my-2 p-2"
+        />
+      </label>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+        onClick={compileModelInfo}
+      >
+        confirm contiue{" "}
+      </button>
+    </form>
+  );
+}
+export default ModelInfo;
